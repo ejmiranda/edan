6d73254434d5fb9479bec08615b1f97eeb9e0368
Edgar Miranda <edgar.j.miranda@me.com>
2018-10-25 11:17:13 -0500

    Add footer style

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
 const year = document.querySelector(`.year`);
+const services = document.querySelector(`.services`);
+const content = document.querySelector(`.content`);
 
 year.textContent = new Date().getFullYear();
+
+//content.style.height = `${services.getBoundingClientRect().bottom}px`;
