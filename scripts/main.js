a4babd3eefa1b6903707b7d583a86760f768b243
Edgar Miranda <edgar.j.miranda@me.com>
2018-10-30 16:57:22 -0500

    Add slideshow functionality with prev/next/dots (missing animation)

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,37 @@
-const year = document.querySelector(`.year`);
+const slides = document.querySelectorAll(`.slide`);
+const prevBtn = document.querySelector(`.previous`);
+const nextBtn = document.querySelector(`.next`);
+const dots = document.querySelectorAll(`.dot`);
 
-year.textContent = new Date().getFullYear();
+let index = 0;
+
+prevBtn.addEventListener(`click`, () => {
+  index = (index > 0) ? --index : slides.length - 1;
+  showSlide(index);
+});
+
+nextBtn.addEventListener(`click`, () => {
+  index = (index < 3) ? ++index : 0;
+  showSlide(index);
+});
+
+for (let [dotIndex, dot] of dots.entries()) {
+  dot.addEventListener(`click`, () => {
+    index = dotIndex;
+    showSlide(index);
+  });
+}
+
+function showSlide(index) {
+  slides.forEach((slide) => slide.classList.remove(`active`));
+  dots.forEach((dot) => dot.classList.remove(`selected`));
+  slides[index].classList.add(`active`);
+  dots[index].classList.add(`selected`);
+}
+
+updateCopyrightYear();
+
+function updateCopyrightYear() {
+  const year = document.querySelector(`.year`);
+  year.textContent = new Date().getFullYear();
+}
